107cd60b3cfad7273ca1d915c203a719628b2687
ashamathi <53160684+ashamathi@users.noreply.github.com>
2019-07-27 18:15:42 +0530

    Add files via upload

---
+++ Carsample.py
@@ -0,0 +1,13 @@
+thisdict={
+ "name":input("owner name:"),
+ "num":input("Enter a vehicle Number:"),
+ "year":input("Year:"),
+ "brand":input("Brand:")
+}
+thisdict["Price"]="8989"
+print(thisdict)
+
+
+ 
+
+ 
